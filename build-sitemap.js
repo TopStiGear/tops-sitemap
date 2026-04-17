d73b0f8eca0a497b39ca1ed432ba3af47adf5213
TopStiGear <topstigear@gmail.com>
2026-04-17 04:52:33 +0100

    Update build-sitemap.js

--- build-sitemap.js
+++ build-sitemap.js
@@ -3,28 +3,36 @@ const fs = require("fs");
 const API_KEY = process.env.YT_KEY;
 const CHANNEL_ID = "UCQKYoj8mNNJw4XQ6xB2gFkA";
 
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("HTTP error " + res.status);
+  return res.json();
+}
+
 async function run() {
-  const channel = await fetch(
-    `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${CHANNEL_ID}&key=${API_KEY}`
-  ).then(r => r.json());
+  try {
+    const channel = await fetchJSON(
+      `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${CHANNEL_ID}&key=${API_KEY}`
+    );
 
-  const uploads = channel.items[0].contentDetails.relatedPlaylists.uploads;
+    const uploads =
+      channel.items[0].contentDetails.relatedPlaylists.uploads;
 
-  const playlist = await fetch(
-    `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${uploads}&maxResults=30&key=${API_KEY}`
-  ).then(r => r.json());
+    const playlist = await fetchJSON(
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${uploads}&maxResults=30&key=${API_KEY}`
+    );
 
-  const urls = playlist.items.map(item => {
-    const id = item.snippet.resourceId.videoId;
+    const urls = playlist.items.map(item => {
+      const id = item.snippet.resourceId.videoId;
 
-    return `
+      return `
   <url>
     <loc>https://tops.stream/videos.html?v=${id}</loc>
     <changefreq>weekly</changefreq>
   </url>`;
-  });
+    });
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://tops.stream/</loc>
@@ -33,7 +41,12 @@ async function run() {
 ${urls.join("\n")}
 </urlset>`;
 
-  fs.writeFileSync("sitemap.xml", sitemap);
+    fs.writeFileSync("sitemap.xml", sitemap);
+    console.log("Sitemap generated successfully");
+  } catch (err) {
+    console.error("Sitemap build failed:", err);
+    process.exit(1);
+  }
 }
 
 run();
